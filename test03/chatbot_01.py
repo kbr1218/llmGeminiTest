06591a11b304ba17aa650ca8cd5cacc7dcbe6a7e
kbr_1218 <kbr_uni@naver.com>
2024-10-21 19:03:01 +0900

    [DELETE] test directory branch로 옮김

--- test03/chatbot_01.py
+++
@@ -1,9 +0,0 @@
-# chatbot_01.py
-import streamlit as st
-
-prompt = st.chat_input("메시지를 입력하세요.")
-if prompt:
-    with st.chat_message("user"):                #  "user"/"human", "assistant"/"ai" 
-        st.write(prompt)
-    with st.chat_message("assistant", avatar="🐯"):
-        st.write("이것은 인공지능 응답입니다.")
