abeddb600df91906850809311366c0de0cef3d23
kbr_1218 <kbr_uni@naver.com>
2024-10-24 10:31:53 +0900

    [STYLE] 시작화면 디자인 끝

--- app.py
+++ app.py
@@ -2,8 +2,8 @@
 import streamlit as st
 
 # 이미지 변수 선언
-titleImgPath = 'https://raw.githubusercontent.com/kbr1218/streamlitTest/main/imgs/title.png'
-botImgPath = 'https://raw.githubusercontent.com/kbr1218/streamlitTest/main/imgs/dolhareubang2.png'
+titleImgPath = 'https://raw.githubusercontent.com/kbr1218/streamlitTest/main/imgs/banner1.png'
+botImgPath = 'https://raw.githubusercontent.com/kbr1218/streamlitTest/main/imgs/dolhareubang3.png'
 
 # 페이지 제목 설정
 st.set_page_config(page_title="시작 페이지", page_icon=":🍊:", layout="wide",
@@ -28,13 +28,14 @@ with open('style/start_page.css', encoding='utf-8') as css_file:
 # 타이틀 이미지
 titleImg = (f"""
 <div class=titleImg>
-    <img src="{titleImgPath}" alt="title image" width="10%">
+    <img src="{titleImgPath}" alt="title image" width=100%>
 </div>
 """)
 st.markdown(titleImg, unsafe_allow_html=True)
 
-st.caption("🚀 2024 빅콘테스트 (생성형 AI분야) 팀: 헬로빅콘")
+st.caption("🚀 2024 빅콘테스트 (생성형 AI 분야) 팀: 헬로빅콘")
 st.markdown("<hr>", unsafe_allow_html=True)
+
 
 # 말풍선
 st.markdown(f"""
@@ -42,14 +43,16 @@ st.markdown(f"""
         <img src="{botImgPath}" class="chat-icon" alt="chatbot">
         <div class="chat-bubble">
             <div class="chat-text">
-                <strong style="color: #C35817;">🏵️친절한 제주도°C:</strong><br>
+                <strong class="color_orange">🏵️친절한 제주도°C:</strong><br>
                 혼저옵서예! <br>
-                제주도 기상청 <strong>'날씨' & '신한카드 제주 가맹점 이용'</strong> 데이터 기반으로 <br>
-                삼춘한테 딱 맞는 맛집들 추천해드릴게마씸 😊
+                신한카드 <strong>'제주 가맹점 이용 데이터'</strong>와 기상청 <strong>'월별 시간별 기온 데이터'</strong> 기반으로<br>
+                삼춘한테 딱 맞는 맛집들 추천해드릴게<span class="color_orange">마씸 🍊</span>
             </div>
         </div>
     </div>
 """, unsafe_allow_html=True)
+st.write("")
+
 
 # 긴 말풍선 테스트
 st.markdown(f"""
@@ -57,12 +60,10 @@ st.markdown(f"""
         <img src={botImgPath} class="chat-icon" alt="chatbot">
         <div class="chat-bubble">
             <div class="chat-text">
-                우리 맛집 추천 서비스 쓰려면 <br>
-                먼저 5가지 묻는 말에 대답해줘야 함서.
-                <br><br>
-                왜냐면, 그 대답에 맞춰서 <strong>맛집</strong>을 추천해줄거라 마씸 <br><br>
-                그럼 묻는 말에 대답 함 해볼까 마씸 <br>
-                출발 하쿠다! 
+                우리 맛집 추천 서비스를 쓰려면 먼저 5가지 질문에 대답해줘야 함서. <br>
+                왜냐면, 그 대답에 맞춰서 <strong>맛집</strong>을 추천해줄거라<span class="color_orange">마씸.</span><br><br>
+                그럼 묻는 말에 대답 함 해볼까<span class="color_orange">마씸.</span> <br>
+                <strong>출발 하쿠다!<strong> 
             </div>
         </div>
     </div>
@@ -72,7 +73,6 @@ st.markdown(f"""
 # 시작하기 버튼 (or 로그인 버튼)
 st.write("")
 start_button = st.page_link("pages/survey.py",
-                              label="**시작하기✈️**",
-                            #   use_container_width=True
-                              )
-
+                            label="[**시작하기✈️**]",
+                            use_container_width=True
+                            )
