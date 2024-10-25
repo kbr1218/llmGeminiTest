ca393e07f0952555d1ec455454177ca1a1a7612b
kbr_1218 <kbr_uni@naver.com>
2024-10-25 11:45:23 +0900

    [FIX] vectorstore 저장 함수

--- pages/functions/set_up_vectorstore.py
+++ pages/functions/set_up_vectorstore.py
@@ -2,11 +2,6 @@
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import Chroma
 from langchain_community.document_loaders.csv_loader import CSVLoader
-from dotenv import load_dotenv
-import pandas as pd
-
-# 환경변수 로드
-load_dotenv()
 
 # 01. CSV 파일에서 문서 로드
 loader = CSVLoader(file_path="data/sample_with_meta.csv",
