08fea4f0addad465e8937e3eec58d81cb0a5ec9a
kbr_1218 <kbr_uni@naver.com>
2024-11-07 17:42:00 +0900

    [FEAT] 데이터 지역 필터링 기능 추가 & prompt 수정중

--- pages/chat.py
+++ pages/chat.py
@@ -1,13 +1,16 @@
 # chat.py
 import streamlit as st
-from langchain_core.runnables import RunnablePassthrough
-from langchain_core.output_parsers import StrOutputParser
+
 from langchain_community.vectorstores import Chroma
 from langchain.embeddings import HuggingFaceEmbeddings
+
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.output_parsers import StrOutputParser
+
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.prompts import ChatPromptTemplate
-from langchain_teddynote import logging
 
+from langchain_teddynote import logging
 from dotenv import load_dotenv
 import os
 
@@ -38,7 +41,7 @@ embeddings  = HuggingFaceEmbeddings(model_name="jhgan/ko-sroberta-multitask")
 
 
 ## 2. Chroma 벡터스토어 로드 ###
-vectorstore = Chroma(persist_directory="./database_ALL_with_meta", embedding_function=embeddings)
+vectorstore = Chroma(persist_directory="./sample_1500_vectorstore", embedding_function=embeddings)
 
 
 ## 3. 사용자 정보 기반 지역 필터링 ###
@@ -48,50 +51,46 @@ visit_dates = st.session_state.get('visit_dates', [])
 visit_times = st.session_state.get('visit_times', [])
 visit_region = st.session_state.get('region', [])
 
-# 필터 조건 구성
-region_filter = {"지역": {"$in": visit_region}}
-print(f"필터링된 지역: {visit_region}")
-print(f"필터 조건: {region_filter}")
-
 
 ## 4. 검색기 생성 ###
 retriever = vectorstore.as_retriever(
     search_type="mmr",   
-    search_kwargs={"k": 10,
-                   "fetch_k": 10,
-                   "filters": region_filter}
+    search_kwargs={"k": 10,              # 반환할 문서 수 (default: 4)
+                   "fetch_k": 50,        # MMR 알고리즘에 전달할 문서 수
+                   "lambda_mult": 0.5}   # 결과 다양성 조절 (default: 0.5)
 )
 
 
 ## 5. 프롬프트 템플릿 설정 (수정 필요: 날씨에 기반하여 대답하도록 수정) ###
 template = """
+You are an assistant for question-answering tasks named '친절한 제주도°C', which recommends good restaurants in Jeju Island based on the given temperature and restaurant infomation data.
+
+Use the following pieces of retrieved context to answer the question.
+If you don't know the answer, just say that you don't know. Recommend three palces maximum and keep the answer concise.
+
+Restaurant Recommendations Criteria: restaurants with a similar temperature in the month of {visit_dates} and the time zone {visit_times} where the user wants to visit has a high percentage/priority of use.
+
 [context]: {context}
 ---
 [질의]: {query}
 ---
 [예시 응답]
-{visit_dates}에 {visit_region} 지역 방문을 계획하신 {user_age}에게 {visit_times}에 방문하기 좋은 맛집을 추천해드리겠습니다!
-추천 기준:
- - 기온: {visit_dates}월 {visit_times}에 평균 기온과 유사한 시가에 방문객이 많았던 맛집을 우선 고려했습니다.
- - 시간대: {visit_times} 이용 건수 비중이 높은 가맹점을 우선 고려했습니다.  
+{user_name}님, {visit_dates}월 {visit_region} {visit_times} 맛집 추천드립니다!
+{visit_dates}월 {visit_region}의 평균 기온은 [평균기온]°C이고, {visit_times} 시간대의 평균 기온은 [시간대 평균기온]°C입니다. 이 시간대 이용 건수 비중이 높은 곳을 고려하여 추천드릴게요.
 
 추천 맛집:
-- **가맹점명**: {visit_dates}월의 평균 기온과 {visit_times}의 평균 기온 데이터를 수치를 알려주며 선택한 이유를 설명
-              : {visit_times}의 이용 건수 비중과 월별/업종별 이용 건수 순위를 이용하여 선택한 이유를 설명.
+1. [**가맹점명**]:
+- {visit_dates}월 {visit_region} 지역의 평균 기온과 유사한 시기에 {visit_times} 시간대 이용 건수 비중이 [이용건수비중]%로 높았습니다.
+- [월별-업종별 이용건수 순위]위를 기록했으며, 연령대 {user_age}의 방문 비율이 [연령별 회원수 비중]%로 비슷한 연령대 고객이 많이 찾습니다.
 
 ----
-[추가 정보]
-당신은 주어진 [context]에 맞게 응답해야 합니다. If you can't find data, say you don't know.
-you must answer based on {visit_dates}, {visit_times} average temperature data or 월별_업종별_이용건수_순위, 시간별 이용건수_비중 data.
-{visit_dates}와 {visit_region}, {visit_times}에 따라 맞춤형 맛집을 2개 또는 3개 추천하고, 이유를 데이터 기반으로 설명하세요, 데이터를 찾지 못한다면 통계를 생략해도 좋습니다.
-
 [데이터 설명]
 {user_age}: 사용자의 연령대,
 {visit_dates}: 사용자가 제주도를 방문하는 기간,
 {visit_times}: 사용자가 맛집을 방문할 시간,
 {visit_region}: 사용자가 방문하는 제주도 지역,
 기준년월-2023년 1월~12월,
-업종-요식관련 30개 업종으로 구분 (업종이 '커피'일 경우 '카페' 뜻함 ),
+업종-요식관련 30개 업종으로 구분 (업종이 '커피'일 경우 '카페'를 뜻함 ),
 지역-제주도를 10개의 지역으로 구분(동부/서부/남부/북부/산지/가파도/마라도/비양도/우도/추자도),
 주소-가맹점 주소,
 월별_업종별_이용건수_순위: 월별 업종별 이용건수 분위수 구간을 6개 구간으로 집계 시 해당 가맹점의 이용건수가 포함되는 분위수 구간 * 1:상위 10%이하 2:상위 10~25% 3:상위 25~50% 4:상위 50~75% 5:상위 75~90% 6:상위 90% 초과(하위 10%이하),
@@ -107,7 +106,7 @@ prompt = ChatPromptTemplate.from_template(template)
 def load_model():
     system_instruction = (
         "당신은 제주도 여행객에게 제주도 맛집을 추천하는 '친절한 제주°C' 챗봇입니다. "
-        "사용자가 사전에 제공한 데이터({user_age}, {visit_dates}, {start_month}, {end_month}, {visit_times}, {visit_region})를 기반으로 얘기하세요."
+        "사용자가 사전에 제공한 데이터(사용자 이름: {user_name}, 연령대: {user_age}, 방문기간: {visit_dates}, 방문 시간대: {visit_times}, 방문 지역: {visit_region})를 기반으로 얘기하세요."
     )
     model = ChatGoogleGenerativeAI(
         model="gemini-1.5-flash",
@@ -117,29 +116,33 @@ def load_model():
     )
     print("model loaded...")
     return model
-model = load_model()
 
 
+### 7. 검색 결과 필터링 & 병합 함수 ###
+# visit_region 데이터 필터링
+def filter_results_by_region(docs, visit_region):
+    return [doc for doc in docs if doc.metadata.get('지역') in visit_region]
 
-### 7. 검색 결과 병합 함수 ###
-def merge_pages(pages):
-    merged = "\n\n".join(page.page_content for page in pages)
-    return merged
+def format_docs(docs):
+  return "\n\n".join(doc.page_content for doc in docs)
 
 
 ## 8. LangChain 체인 구성 ###
-chain = (
-    {"query": RunnablePassthrough(),
-     "context": retriever | merge_pages,      # retriever로 검색된 문서를 merge_pages 함수에 전달
-     "visit_dates":RunnablePassthrough(),
-     "visit_times":RunnablePassthrough(),
-     "visit_region":RunnablePassthrough(),
-     "user_age":RunnablePassthrough(),
-     "user_name":RunnablePassthrough()
-    }
-    | prompt
-    | load_model()
-    | StrOutputParser()
+rag_chain = (
+  {"context": retriever
+   | (lambda docs: filter_results_by_region(docs, visit_region))
+   | format_docs(),
+    "query":RunnablePassthrough(),
+    "user_name":RunnablePassthrough(),
+    "user_age":RunnablePassthrough(),
+    "visit_dates":RunnablePassthrough(),
+    "visit_times":RunnablePassthrough(),
+    "visit_region":RunnablePassthrough()
+  }
+  # question(사용자의 질문) 기반으로 연관성이 높은 문서 retriever 수행 >> format_docs로 문서를 하나로 만듦
+  | prompt               # 하나로 만든 문서를 prompt에 넘겨주고
+  | load_model()         # llm이 원하는 답변을 만듦
+  | StrOutputParser()
 )
 
 
@@ -174,7 +177,7 @@ with chat_col1:
         ]
     # 필수 정보가 입력되지 않았을 경우 오류 메시지 출력
     if not (user_age and visit_dates and visit_times and visit_region):
-        st.error("사용자 정보(나이, 방문 날짜, 시간, 지역)가 누락되었습니다. 사이드바에서 정보를 입력해 주세요.")
+        st.error("사용자 정보(연령대, 방문 날짜, 시간, 지역)가 누락되었습니다. \n왼쪽 사이드바에서 정보를 입력해 주세요.")
         st.stop()  # 이후 코드를 실행하지 않도록 중단
 
 
@@ -191,7 +194,7 @@ with chat_col1:
         # 추천 생성 중 스피너
         with st.spinner("맛집 찾는 중..."):
             # chain.invoke에서 개별 변수로 전달
-            assistant_response = chain.invoke(user_input+f"""
+            assistant_response = rag_chain.invoke(user_input+f"""
                                               user_name: {user_name},
                                               user_age: {user_age},
                                               visit_region: {visit_region},
